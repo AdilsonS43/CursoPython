572d0df86b5260ab234efb843da5821d8c3e1900
Adilson <adilson43.as@gmail.com>
2025-05-26 18:52:32 -0300

    upload de exercicios

---
+++ inicio.py
@@ -0,0 +1,16 @@
+nome = input('Qual seu nome?')
+print('Seja bem vindo!' , nome)
+
+print("#############################################")
+
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu dia' , dia, 'do mês ' ,mes , 'do ano ' , ano)
+
+print("#############################################")
+
+num1 = input('Digite um número ')
+num2 = input('Digite outro número ')
+soma = int(num1) + int(num2)
+print('A soma dos números é', soma)
